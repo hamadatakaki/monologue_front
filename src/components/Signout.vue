291c1bcf48436d19d7827980b881789ee1e50460
hamadatakaki <i16hamadat@gmail.com>
2019-02-23 00:57:19 +0900

    :tada: implemented tiny route guard with router object!!

---
+++ src/components/Signout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Sign out</h1>
+        <form>
+            <input type="button" @click="logout">
+        </form>
+        <a href="#/signin/">to signin</a>
+    </div>
+</template>
+<style lang="scss" src="./scss/signin.scss"></style>
+<script>
+    import controller from "./js/controller";
+
+    export default {
+        data() {
+            return {
+                userName: ""
+            };
+        },
+        methods: {
+            logout() {
+                localStorage.token = ""
+            }
+        }
+    }
+</script>
